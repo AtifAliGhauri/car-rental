debc8a64415d1c042f45a449ff08b6f28b2ca18a
Atif Ali Ghauri <atifalighouri@gmail.com>
2024-03-03 08:45:01 +0500

    first-commit

---
+++ src/pages/BlogDetails.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect } from 'react'
+import {Container, Row, Col, Form, FormGroup, Input} from 'reactstrap'
+import { Link, useParams } from 'react-router-dom'
+import blogData from '../assets/data/blogData'
+import Helmet from '../components/Helmet/Helmet'
+import commentImg from '../assets/all-images/ava-1.jpg'
+
+import '../styles/blog-details.css'
+
+const BlogDetails = () => {
+
+  const {slug} = useParams()
+  const blog = blogData.find(blog => blog.title === slug)
+
+  useEffect(()=>{
+    window.scrollTo(0,0);
+  },[blog])
+
+  return <Helmet title={blog.title}>
+    <Container className='mt-5'>
+      <Row>
+        <Col lg='8' md='8'>
+          <div className='blog__details'>
+            <img src={blog.imgUrl} alt='' className='w-100' />
+            <h2 className='section__title'>{blog.title}</h2>
+
+            <div className='blog__publisher d-flex align-items-center gap-4 mb-4'>
+                <span className='blog__author'>
+                  <i className='ri-user-line'></i>{blog.author}
+                </span>
+             
+                <span className='d-flex align-items-center gap-1 section__description'>
+                   <i class="ri-calendar-line"></i>{blog.date}
+                </span>
+
+                <span className='d-flex align-items-center gap-1 section__description'>
+                    <i className='ri-time-line'></i>{blog.time}
+                </span>
+              
+            </div> 
+            <p className='section__description'>{blog.description}</p>     
+            <h6 className='ps-5 fw-normal'><blockquote className='fs-4'>{blog.quote}</blockquote></h6>
+            <p className='section__description'>{blog.description}</p>
+          </div>
+
+          <div className='comment__list mt-5 mb-5'>
+            <h4 className='mb-5'>3  Comments</h4>
+            <div className='single__comment d-flex gap-3'>
+            <img src={commentImg} alt='' />
+              <div className='comment__content'>
+                <h6 className='fw-bold'>David Visa</h6>
+                <p className='section__description mb-0'>14 Feb, 2024</p>
+                <p className='section__description'>
+                  Lorem ipsum is placeholder text commonly used in
+                  the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                </p>
+                <span className='replay d-flex align-items-center gap-1'>
+                <i class="ri-reply-line"></i>Reply
+                </span>
+              </div>
+            </div>
+
+            {/* ================== comment form ============== */}
+
+            <div className='leave__comment-form mt-5'>
+              <h4>Leave a Comment</h4>
+              <p className='section__description'>
+                You must sign-in to make or comment a post
+              </p>
+
+              <Form>
+                <FormGroup className='d-flex gap-3'>
+                  <Input type='text' placeholder='Full Name' />
+                  <Input type='email' placeholder='Email' />
+                </FormGroup>
+
+                <FormGroup>
+                  <textarea rows={5} className='w-100 py-2 px-3' placeholder='Comment...'> </textarea>
+                </FormGroup>
+
+                <button className='btn comment__btn mt-3'>Post a Comment</button>
+              </Form>
+            </div>
+
+          </div>
+        </Col>
+
+        <Col lg='4' md='4'>
+          <div className='recent__post mb-4'>
+            <h5 className='fw-bold'>Recent Post</h5>
+          </div>
+          {
+            blogData.map(item=>(
+              <div className='recent__blog-post mb-4' key={item.id}>
+                <div className='recent__blog-item d-flex gap-3'>
+                  <img src={item.imgUrl} alt='' className='w-25 rounded-2' />
+                  <h6>
+                    <Link to={`/blogs/${item.title}`}>{blog.title}</Link>
+                  </h6>
+                </div>
+              </div>
+            ))
+          }
+        </Col>
+      </Row>
+    </Container>
+  </Helmet>
+}
+
+export default BlogDetails
---
+++ src/pages/Home.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import HeroSlider from '../components/UI/HeroSilder'
+import Helmet from '../components/Helmet/Helmet'
+import FindCarForm from '../components/UI/FindCarForm'
+
+import { Container, Row, Col } from 'reactstrap'
+import AboutSection from '../components/UI/AboutSection'
+import ServiceList from '../components/UI/ServiceList'
+import carData from '../assets/data/carData'
+import CarItem from '../components/UI/CarItem'
+import BecomeDriverSection from '../components/UI/BecomeDriverSection'
+import Testimonial from '../components/UI/Testimonial'
+import BlogList from '../components/UI/BlogList'
+
+const Home = () => {
+  return (
+    <Helmet title="Home">
+
+    {/* ========= hero section ========= */}
+      <section className='p-0 hero__slider-section'>
+        <HeroSlider />
+
+        <div className='hero__form'>
+          <Container>
+            <Row className='form__row'>
+              <Col lg='4' md='4'>
+                <div className='find__cars-left'>
+                  <h2>Find your best car here</h2>
+                </div>
+              </Col>
+
+              <Col lg='8' md='8' sm='12'>
+                <FindCarForm />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </section>
+
+      {/* ========== about section =========== */}
+
+      <AboutSection />
+
+      {/* ===========  services section =========== */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12' className='mb-5 text-center'>
+              <h6 className='section__subtitle'>See Our</h6>
+              <h2 className='section__title'>Popular Services</h2>
+            </Col>
+
+            <ServiceList />
+          </Row>
+        </Container>
+      </section>
+
+    {/* ============= car offer section ================ */}
+
+    <section>
+      <Container>
+        <Row>
+          <Col lg='12' className='text-center mb-5'>
+            <h6 className='section__subtitle'>Come with </h6>
+            <h2 className='section__title'>Hot Offers</h2>
+          </Col>
+
+          {
+            carData.slice(0, 6).map((item) => (
+              <CarItem item={item} key={item.id} />
+            ))
+          }
+
+        </Row>
+      </Container>
+    </section>
+
+          {/* ============= become a driver Section ========= */}
+        <BecomeDriverSection />
+
+        {/* ==============  testimonial section ============ */}
+
+        <section>
+          <Container>
+            <Row>
+              <Col lg='12' className='mb-4 text-center'>
+                <h6 className='section__subtitle'>Our Clients says</h6>
+                <h2 className='section__title'>Testimonial</h2>
+              </Col>
+
+              <Testimonial />
+            </Row>
+          </Container>
+        </section>
+
+        {/* ==============  blog Section ============= */}
+
+        <section>
+          <Container>
+            <Row>
+              <Col lg='12' className='mb-5 text-center'>
+                <h6 className='section__subtitle'>Explore Our Blogs</h6>
+                <h2 className='section__title'> Latest Blogs</h2>
+              </Col>
+
+              <BlogList />
+            </Row>
+          </Container>
+        </section>
+
+    </Helmet>
+  )
+}
+
+export default Home
